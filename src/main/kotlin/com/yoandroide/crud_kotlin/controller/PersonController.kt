50b59166887d45344a0ccdd9e11f5ee05f208bd9
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2026-02-01 14:04:57 -0500

    update imports with *

--- src/main/kotlin/com/yoandroide/crud_kotlin/controller/PersonController.kt
+++ src/main/kotlin/com/yoandroide/crud_kotlin/controller/PersonController.kt
@@ -5,8 +5,14 @@ import com.yoandroide.crud_kotlin.controller.responses.PersonResponse
 import com.yoandroide.crud_kotlin.service.PersonService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 
 @RestController
@@ -37,6 +43,11 @@ class PersonController (private val personService: PersonService) {
         } else {
             ResponseEntity.notFound().build()
         }
+    }
+
+    @GetMapping("/status")
+    fun testService(): ResponseEntity<String>{
+        return ResponseEntity.ok("UP")
     }
 
 }
